c5c8927e3b20c1594be8e8bb9de6e4e718988e5b
niuqinghua <niuqinghua@letv.com>
2015-07-28 23:18:40 +0800

    add samples

---
+++ diamond-samples/diamond-service-module/src/main/java/dream/diamond/sample/controller/SampleController.java
@@ -0,0 +1,24 @@
+package dream.diamond.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dream.diamond.service.SampleService;
+
+/**
+ * Created by niuqinghua on 2015/7/24.
+ */
+@RestController
+@RequestMapping(value = "/sample")
+public class SampleController {
+
+    @Autowired
+    private SampleService sampleService;
+
+    @RequestMapping("/test")
+    public String test() {
+        return sampleService.test();
+    }
+
+}
