a6939ebd1d9a21cdb1b83f3552a0032b3e38c4a3
niuqinghua <nqh_2005@163.com>
2015-09-14 21:43:24 +0800

    add swiftMq configuration

---
+++ diamond-autoconfigure/src/main/java/dream/diamond/autoconfigure/swiftmq/SwiftMQAutoConfiguration.java
@@ -0,0 +1,7 @@
+package dream.diamond.autoconfigure.swiftmq;
+
+/**
+ * Created by niuqinghua on 15/9/14.
+ */
+public class SwiftMQAutoConfiguration {
+}
---
+++ diamond-autoconfigure/src/main/java/dream/diamond/autoconfigure/swiftmq/SwiftMQProperties.java
@@ -0,0 +1,11 @@
+package dream.diamond.autoconfigure.swiftmq;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by niuqinghua on 15/9/14.
+ */
+@ConfigurationProperties(prefix = "diamond.activemq")
+public class SwiftMQProperties {
+
+}
