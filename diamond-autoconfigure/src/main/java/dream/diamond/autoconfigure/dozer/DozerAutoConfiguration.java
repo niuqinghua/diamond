ab313b6d7ddde64110224d2dd66efe25282e4585
niuqinghua <nqh_2005@163.com>
2015-10-11 16:47:07 +0800

    add dozer configuration

---
+++ diamond-autoconfigure/src/main/java/dream/diamond/autoconfigure/dozer/DozerAutoConfiguration.java
@@ -0,0 +1,21 @@
+package dream.diamond.autoconfigure.dozer;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by niuqinghua on 15/10/11.
+ */
+@Configuration
+@ConditionalOnClass(DozerBeanMapper.class)
+public class DozerAutoConfiguration {
+
+    @ConditionalOnMissingBean
+    @Bean(name = "dozerBeanMapper")
+    public DozerBeanMapper dozerBeanMapper() {
+        return new DozerBeanMapper();
+    }
+
+}
